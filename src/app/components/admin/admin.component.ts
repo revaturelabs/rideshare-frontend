1f2160110b962c83d38eed5be2eb0a25f382b4f3
Peter Nguyen <peter.nguyen17@gmail.com>
2020-01-07 11:57:09 -0500

    added tsconfig.app.json inorder for compodoc to serve the doc pages

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -7,12 +7,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
+  /**
+   * @constructor 
+   * @param router Provides an instance of a router 
+   */
   constructor(private router: Router) { }
+
+  /**
+   * This is an OnInit function that is evoked once.
+   */
 
   ngOnInit() {
   }
-
+  /**
+   * Function that takes no parameters.
+   * 
+   * @return {void} Clears the session storage and logs out.
+   * 
+   */
   logout() {
     sessionStorage.clear();
     this.router.navigate(['']);

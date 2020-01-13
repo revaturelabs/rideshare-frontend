945cdf6beb4ed2522efdb934b79f24964cf6fb51
cwcheung <carl.w.cheung@gmail.com>
2020-01-13 16:48:54 -0500

    add component to handle logging

---
+++ src/app/components/logger/logger.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { log } from '../services/log.service'; 
+
+@Component({
+  selector: 'app-logger',
+  templateUrl: './logger.component.html',
+  styleUrls: ['./logger.component.css']
+})
+export class LoggerComponent implements OnInit {
+  logStatement  = "no logs";
+  constructor(private logService: log) { }
+
+  debugClicked(message) {
+    this.logStatement = this.logService.debug(message);
+  }
+  warnClicked(message) {
+    this.logStatement = this.logService.warn(message); 
+  }
+  errorClicked(message) {
+    this.logStatement = this.logService.error(message); 
+  }
+  infoClicked(message) {
+    this.logStatement = this.logService.info(message); 
+  }
+
+  ngOnInit() {
+    this.logStatement = "start logger"; 
+    this.logService.info(this.logStatement); 
+  }
+
+}

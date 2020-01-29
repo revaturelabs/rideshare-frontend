4c732aeab69b35488e10888522603a98e378030f
flubbid <brendanwilson1@gmail.com>
2020-01-29 10:01:11 -0500

    adds new components

---
+++ src/app/components/driver-list/driver-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-list',
+  templateUrl: './driver-list.component.html',
+  styleUrls: ['./driver-list.component.css']
+})
+export class DriverListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

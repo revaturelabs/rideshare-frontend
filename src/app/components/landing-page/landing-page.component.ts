27a33f13768f594dceba210aacaad0e894f5d897
RDixon540 <remington.dixon@gmail.com>
2020-02-07 10:55:49 -0500

    message1

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,31 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import {} from 'googlemaps';
 import { ViewChild } from '@angular/core';
-
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
-
-
   @ViewChild('map', {static: true}) mapElement: any;
     map: google.maps.Map;
   constructor() { }
-
-  
-
+  lat = 40.730610;
+  lng = -73.935242;
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+  mapOptions: google.maps.MapOptions = {
+    center:this.coordinates,
+    zoom:10
+  }
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map,
+  });
   ngOnInit(): void {
-    const mapProperties = {
-         center: new google.maps.LatLng(35.2271, -80.8431),
-         zoom: 12,
-         mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+    this.mapInitializer()
  }
+ mapInitializer() {
+  this.map = new google.maps.Map(this.mapElement.nativeElement, 
+  this.mapOptions);
+  this.marker.setMap(this.map);
+}
  
-
+ searchDrivers(){
+ }
   
-
 }

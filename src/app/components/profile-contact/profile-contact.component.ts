f13ce3898e8e03ee5339c30a7f921303c842de23
WilliamFoltz <60475228+WilliamFoltz@users.noreply.github.com>
2020-03-04 08:51:29 -0500

    update

--- src/app/components/profile-contact/profile-contact.component.ts
+++ src/app/components/profile-contact/profile-contact.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user-service/user.service';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
+import { EmployeeServiceService } from 'src/app/services/employee-service.service';
+import { Employee } from 'src/app/Models/Employee';
 
 @Component({
   selector: 'app-profile-contact',
@@ -12,33 +14,41 @@ export class ProfileContactComponent implements OnInit {
 
   profileObject : User;
   currentUser: any = '';
+  currentEmployee: Employee;
+  currentId: any;
   firstName: string;
   lastName: string;
   email: string;
   phone: string;
   success :string;
-  constructor(private router: Router, private userService: UserService) { }
+  constructor(private router: Router, private userService: UserService, private employeeService: EmployeeServiceService) { }
 
   ngOnInit() {
-    this.currentUser = this.userService.getUserById2(sessionStorage.getItem("userid")).subscribe((response)=>{
-      this.profileObject = response;
+   
+    this.currentEmployee = JSON.parse(sessionStorage.getItem("User"));
+    // this.currentId = this.employeeService.getEmployeeById(this.currentEmployee.employee_id);
+    this.firstName = this.currentEmployee.first_name;
+    this.lastName = this.currentEmployee.last_name;
+    this.email = this.currentEmployee.email;
+    this.phone = this.currentEmployee.phone_number;
+    // this.currentUser = this.userService.getUserById2(sessionStorage.getItem("userid")).subscribe((response)=>{
+    // this.profileObject = response;
+    // this.firstName = this.profileObject.firstName;
+    // this.lastName = this.profileObject.lastName;
+    // this.email = this.profileObject.email;
+    // this.phone = this.profileObject.phoneNumber;
 
-      this.firstName = this.profileObject.firstName;
-      this.lastName = this.profileObject.lastName;
-      this.email = this.profileObject.email;
-      this.phone = this.profileObject.phoneNumber;
-
-    });
+    // });
     
   }
 
   updatesContactInfo(){
-    this.profileObject.firstName = this.firstName;
-    this.profileObject.lastName = this.lastName;
-    this.profileObject.email = this.email;
-    this.profileObject.phoneNumber = this.phone;
+    this.currentEmployee.first_name = this.firstName;
+    this.currentEmployee.last_name = this.lastName;
+    this.currentEmployee.email = this.email;
+    this.currentEmployee.phone_number = this.phone;
 
-    this.userService.updateUserInfo(this.profileObject);
+    this.employeeService.updateEmployee(this.currentEmployee);
     this.success = "Updated Successfully!";
   }
 
---
+++ src/app/manager-page/manager-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-page',
+  templateUrl: './manager-page.component.html',
+  styleUrls: ['./manager-page.component.css']
+})
+export class ManagerPageComponent implements OnInit {
+
+  showManagerPage:boolean;
+  showEditOffice:boolean;
+  showManagerEditEmployee:boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.showManagerPage = true;
+    this.showEditOffice = false;
+    this.showManagerEditEmployee = false;
+  }
+
+  editEmployee(){
+
+
+    this.showManagerPage = false;
+    this.showManagerEditEmployee = true;
+  }
+
+  editLocation(){
+
+
+    this.showManagerPage = false;
+    this.showEditOffice = true;
+  }
+
+}

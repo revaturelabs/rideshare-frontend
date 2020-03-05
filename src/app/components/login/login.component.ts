7e872aa8e7a0f08b16416b45a4ccd095b7b53b77
rhtydv <rhtydv@gmail.com>
2020-03-05 12:56:57 -0600

    extra stuff deleted

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, NgModule, TemplateRef } from '@angular/core';
 // import { UserService } from 'src/app/services/user-service/user.service';
-import { User } from '../../models/user';
+
 // import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { AuthService } from '../../services/auth-service/auth.service';
+// import { AuthService } from '../../services/auth-service/auth.service';
 import { Router, RouterModule } from '@angular/router';
 import { BsModalService, BsModalRef} from 'ngx-bootstrap';
 import { EmployeeServiceService } from '../../services/employee-service.service';
@@ -31,10 +31,7 @@ export class LoginComponent implements OnInit {
 	 * Sets pagination
 	 */
 
-	users: User[] = [];
-	allUsers: User[] = [];
 
-	chosenUser: User;
 	chosenUserFullName: string = '';
 	userName: string;
 	passWord: string;
@@ -57,75 +54,13 @@ export class LoginComponent implements OnInit {
 	 * @param authService An auth service is injected.
 	 *
 	 */
-	constructor(private modalService :BsModalService,private es: EmployeeServiceService, private http: HttpClient, private authService: AuthService, public r: Router) { }
+	constructor(private modalService :BsModalService,private es: EmployeeServiceService, private http: HttpClient, public r: Router) { }
 
 	/**
 	 * When the component is initialized, the system checks for the session storage to validate. Once validated, the user service is called to retrieve all users.
 	 */
 	ngOnInit() {
 		
-	}
-
-	/**
-	 * A function that allows the user to choose an account to log in as
-	 * @param user
-	 */
-
-	// changeUser(user) {
-	// 	this.showDropDown = false;
-	// 	this.curPage = 1;
-	// 	this.totalPage = Math.ceil(this.allUsers.length / 5);
-	// 	this.users = this.allUsers.slice(this.curPage * 5 - 5, this.curPage * 5);
-	// 	this.chosenUserFullName = `${user.firstName} ${user.lastName}: ${user.driver ? 'Driver' : 'Rider'}`;
-	// 	this.chosenUser = user;
-	// }
-
-	/**
-	 * A GET method the fetches all the users
-	 */
-
-	searchAccount() {
-		this.showDropDown = true;
-		if (this.chosenUserFullName.length) {
-			this.users = this.allUsers.filter(user => {
-				return (
-					user.firstName.toLowerCase().startsWith(this.chosenUserFullName.toLowerCase()) ||
-					user.lastName.toLowerCase().startsWith(this.chosenUserFullName.toLowerCase()) ||
-					`${user.firstName} ${user.lastName}`.toLowerCase().startsWith(this.chosenUserFullName.toLowerCase()) ||
-					`${user.firstName} ${user.lastName}: ${user.driver ? 'Driver' : 'Rider'}`.toLowerCase().startsWith(this.chosenUserFullName.toLowerCase())
-				);
-			});
-			this.totalPage = Math.ceil(this.users.length / 5);
-		} else {
-			this.curPage = 1;
-			this.totalPage = Math.ceil(this.allUsers.length / 5);
-			this.users = this.allUsers.slice(this.curPage * 5 - 5, this.curPage * 5);
-		}
-	}
-
-	/**
-	 * A toggle function
-	 */
-
-	toggleDropDown() {
-		this.showDropDown = !this.showDropDown;
-	}
-
-	/**
-	 * Set next page
-	 */
-	nextPage() {
-		this.curPage++;
-		this.users = this.allUsers.slice(this.curPage * 5 - 5, this.curPage * 5);
-	}
-
-	/**
-	 * Set prev page
-	 */
-
-	prevPage() {
-		this.curPage--;
-		this.users = this.allUsers.slice(this.curPage * 5 - 5, this.curPage * 5);
 	}
 
 	/**
@@ -166,50 +101,5 @@ export class LoginComponent implements OnInit {
 	}
 
 
-
-
-
-	// login() {
-	// 	this.pwdError ='';
-	// 	this.usernameError= '';
-		
-    //     this.http.get(`${environment.loginUri}?userName=${this.userName}&passWord=${this.passWord}`)
-	// 		.subscribe(
-    //               (response) => {
-    //                  //console.log(response);
-    //                   if(response["userName"] != undefined){
-    //                      this.usernameError=  response["userName"][0];
-    //                   }
-    //                   if(response["passWord"] != undefined){
-    //                      this.pwdError = response["pwdError"][0];
-	// 				  }
-	// 				  if((response["name"] != undefined) && (response["userid"] != undefined)){
-	// 					sessionStorage.setItem("name", response["name"]);
-	// 					sessionStorage.setItem("userid", response["userid"]);
-						
-	// 					//call landing page
-	// 					//this.router.navigate(['landingPage']);
-	// 					location.replace('profile');
-	// 				  }
-	// 				  if(response["userNotFound"] != undefined){
-	// 					this.userNotFound = response["userNotFound"][0];
-	// 				  }
-    //              }
-    //     );
-		/*this.http.get<User[]>(`${environment.userUri}?username=${this.userName}`)
-			.subscribe((user: User[]) => {
-				if (!user.length) {
-					this.loginFailed();
-				}
-				else if(this.chosenUser.active == false){
-					this.loginBanned();
-				}
-				else {
-					if (!this.authService.login(user[0], this.chosenUser.userName)) {
-						this.loginFailed();
-					}
-				}
-			});*/
-	// }
 
 

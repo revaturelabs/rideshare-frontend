ea35f763c08b0d2567d39209cc95bb695f397f4a
WaluigiPR <36483683+WaluigiPR@users.noreply.github.com>
2020-03-06 11:04:01 -0500

    Registration validation implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent} from './components/home-page/home-page.component'; //this is home
 import { ProfileComponent } from './components/profile/profile.component'; //this is employee-page
-import { DriverListComponent } from './components/driver-list/driver-list.component';
+import { DriverListComponent } from './Components/driver-list/driver-list.component';
 
 const routes: Routes = [
 {path: "home", component: HomePageComponent},

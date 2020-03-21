e94cab7667cb00ccbbf5e8b45e0bb8b36d5e9113
Daniel <chateaustdanielle@gmail.com>
2020-03-21 19:21:43 -0400

    Membership profile page updates upon clicking save button

--- src/app/components/profile-membership/profile-membership.component.ts
+++ src/app/components/profile-membership/profile-membership.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user-service/user.service';
 import { User } from 'src/app/models/user';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-profile-membership',
   templateUrl: './profile-membership.component.html',
   styleUrls: ['./profile-membership.component.css']
 })
 export class ProfileMembershipComponent implements OnInit {
-  profileObject : User;
+  profileObject : User = new User();
   currentUser: any = '';
   isDriver: boolean;
   active: boolean;
   success: string;
-  constructor(private userService: UserService) { }
+  constructor(private router: Router, private userService: UserService) { }
   ngOnInit() {
     this.currentUser = this.userService.getUserById2(sessionStorage.getItem("userid")).subscribe((response)=>{
       this.profileObject = response;
@@ -21,7 +23,9 @@ export class ProfileMembershipComponent implements OnInit {
   updatesMembershipInfo(){
     this.profileObject.isDriver = this.isDriver;
     this.profileObject.active = this.active;
+    
     this.userService.updateUserInfo(this.profileObject);
     this.success = "Updated Successfully!";
+    
   }
 }

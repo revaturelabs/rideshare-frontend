121ca7f44e469de174e4687e288f6f3b3593c856
Alexander Hartson <alexander-hartson@live.com>
2020-03-26 10:16:38 -0400

    trying to get some car info in a table bruh

--- src/app/components/driver-list/driver-list.component.ts
+++ src/app/components/driver-list/driver-list.component.ts
@@ -22,6 +22,13 @@ export class DriverListComponent implements OnInit {
   availableCars: Array<any> = [];
   drivers: Array<any> = [];
 
+  // TODO: Added variables to make getCarByUserId2 work?
+  make: string;
+  model: string;
+  nrSeats: number;
+  currentCar: Car;
+  success: string;
+
   @ViewChild('map', null) mapElement: any;
   map: google.maps.Map;
 
@@ -30,6 +37,15 @@ export class DriverListComponent implements OnInit {
 
   ngOnInit() {
     this.drivers = [];
+
+    // TODO: test out ngOninit to get car info to display on this component
+    this.carService.getCarByUserId2(sessionStorage.getItem("userid")).subscribe((response)=>{
+      this.currentCar = response;
+      this.make = response.make;
+      this.model = response.model;
+      this.nrSeats = response.seats;
+
+    });
 
     this.userService.getRidersForLocation1(this.location).subscribe(res => {
       // console.log(res);
@@ -161,7 +177,7 @@ export class DriverListComponent implements OnInit {
                                   <td class="col">${results[0].distance.text}</td>
                                   <td class="col">${results[0].duration.text}</td>
 
-                                  <td class="col">${seats}</td>  
+                                  <td class="col">${seats}</td>
 
                                   <td class="col">
                                   <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCentered${element.id}"> View</button>

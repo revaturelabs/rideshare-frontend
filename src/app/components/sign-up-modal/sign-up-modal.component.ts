0f159866c4101837babce2ab012fd458bd03fb4e
Jordan Zech <s521476@nwmissouri.edu>
2020-03-26 12:37:44 -0500

    added autocomplete to signup

--- src/app/components/sign-up-modal/sign-up-modal.component.ts
+++ src/app/components/sign-up-modal/sign-up-modal.component.ts
@@ -5,6 +5,10 @@ import { User } from 'src/app/models/user';
 import { Batch } from 'src/app/models/batch';
 import { BatchService } from 'src/app/services/batch-service/batch.service';
 import { ValidationService } from 'src/app/services/validation-service/validation.service';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { } from 'googlemaps';
+declare var google: any;
 
 @Component({
   selector: 'signupmodal',
@@ -34,6 +38,7 @@ export class SignupModalComponent implements OnInit {
   hStateError: string;
   hCityError: string;
   hZipError: string;
+  autocomplete: google.maps.places.Autocomplete;
 
   success: string;
   //Store the retrieved template from the 'openModal' method for future use cases.
@@ -42,11 +47,13 @@ export class SignupModalComponent implements OnInit {
     'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY',
     'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV',
     'WI', 'WY'];
-  constructor(private modalService: BsModalService, private userService: UserService, private batchService: BatchService, private validationService: ValidationService) { }
+  constructor(private http: HttpClient, private modalService: BsModalService, private userService: UserService, private batchService: BatchService, private validationService: ValidationService) { }
 
   ngOnInit() {
+  
     this.userService.getAllUsers().subscribe(
       res => {
+
         //console.log(res);
       }
     );
@@ -60,8 +67,89 @@ export class SignupModalComponent implements OnInit {
   //Opens 'sign up' modal that takes in a template of type 'ng-template'.
 
   openModal(template: TemplateRef<any>) {
+     
     this.modalRef = this.modalService.show(template);
+    this.getGoogleApi();
   }
+
+  initAutocomplete(): void {
+    console.log('what is going on?');
+    var self = this;
+    // Create the autocomplete object, restricting the search predictions to
+    // geographical location types.
+    var autocomplete = new google.maps.places.Autocomplete(<HTMLInputElement>document.getElementById('autocomplete'), { types: ['geocode'] });
+
+    // Avoid paying for data that you don't need by restricting the set of
+    // place fields that are returned to just the address components.
+    autocomplete.setFields(['address_component']);
+
+    // When the user selects an address from the drop-down, populate the
+    // address fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+
+    function fillInAddress(): void {
+      // Get the place details from the autocomplete object.
+      var place = autocomplete.getPlace();
+
+      var componentForm = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+        postal_code: 'short_name'
+      };
+
+      for (var component in componentForm) {
+        (<HTMLInputElement>document.getElementById(component)).value = '';
+      }
+
+      // Get each component of the address from the place details,
+      // and then fill-in the corresponding field on the form.
+      for (var i = 0; i < place.address_components.length; i++) {
+        var addressType = place.address_components[i].types[0];
+        if (componentForm[addressType]) {
+          var val = place.address_components[i][componentForm[addressType]];
+          (<HTMLInputElement>document.getElementById(addressType)).value = val;
+          switch (addressType) {
+            case "locality":
+              self.user.hCity = val;
+              break;
+            case "administrative_area_level_1":
+              self.user.hState = val;
+              break;
+            case "postal_code":
+              self.user.hZip = val;
+              break;
+          }
+        }
+      }
+      // Create a proper address by combining street_number and route
+      self.address = place.address_components[0]['long_name'] + ' ' +
+        place.address_components[1]['long_name'];
+      (<HTMLInputElement>document.getElementById('address')).value =
+        place.address_components[0]['long_name'] + ' ' +
+        place.address_components[1]['long_name'];
+      self.user.hAddress = place.address_components[0]['long_name'] + ' ' +
+        place.address_components[1]['long_name'];
+    }
+  }
+
+  // Bias the autocomplete object to the user's geographical location,
+  // as supplied by the browser's 'navigator.geolocation' object.
+  geolocate(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var geolocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        var circle = new google.maps.Circle(
+          { center: geolocation, radius: position.coords.accuracy });
+        this.autocomplete.setBounds(circle.getBounds());
+      });
+    }
+  }
+
 
   submitUser() {
     this.user.userId = 0;
@@ -148,5 +236,26 @@ export class SignupModalComponent implements OnInit {
       }*/
     );
 
+  }
+
+  getGoogleApi() {
+    let self = this;
+    this.http.get(`${environment.loginUri}getGoogleApi`)
+      .subscribe(
+      (response) => {
+        //console.log(response);
+        if (response["googleMapAPIKey"] != undefined) {
+          new Promise((resolve) => {
+            let script: HTMLScriptElement = document.createElement('script');
+            script.addEventListener('load', r => resolve());
+            script.src = `http://maps.googleapis.com/maps/api/js?libraries=places&key=${response["googleMapAPIKey"][0]}`;
+            document.head.appendChild(script);
+
+          }).then(resp => {
+            this.initAutocomplete();
+          });
+        }
+      }
+      );
   }
 }

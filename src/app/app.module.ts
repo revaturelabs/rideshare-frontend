be9d464a80f472a53d17934ae71002641dd7633e
Israel Garcia <ingarcia@miners.utep.edu>
2020-03-26 16:08:44 -0500

    Added logger to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-
-
+import { LogService } from 'src/app/services/log.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ModalModule } from 'node_modules/ngx-bootstrap';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DriverComponent} from './components/driver/driver.component';
@@ -85,7 +84,8 @@ import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
     BatchService,
     AuthService,
     ValidationService,
-    BsModalService
+    BsModalService,
+    LogService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -3,7 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class LogService {
+  
   /**
    * A function that logs a debug message
    * @param message 

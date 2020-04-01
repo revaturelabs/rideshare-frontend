208215cbabec35411b8de3da74478ce38c0ef2e5
Mitch Goshorn <msgoshorn@gmail.com>
2020-03-31 23:48:30 -0400

    Copied styling info from signup-modal to signup-component to keep theme consistent.  Removed unused imports from signup-modal

--- src/app/components/sign-up-modal/sign-up-modal.component.ts
+++ src/app/components/sign-up-modal/sign-up-modal.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit, TemplateRef, ViewChild, ElementRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap';
-import { UserService } from 'src/app/services/user-service/user.service';
-import { User } from 'src/app/models/user';
-import { Batch } from 'src/app/models/batch';
-import { BatchService } from 'src/app/services/batch-service/batch.service';
-import { ValidationService } from 'src/app/services/validation-service/validation.service';
-import { MapsAPILoader } from '@agm/core';
 
 @Component({
     selector: 'signupmodal',
@@ -14,15 +8,15 @@ import { MapsAPILoader } from '@agm/core';
 })
 export class SignupModalComponent implements OnInit {
 
-    //Store the retrieved template from the 'openModal' method for future use cases.
+    // Store the retrieved template from the 'openModal' method for future use cases.
     modalRef: BsModalRef;
 
     constructor(private modalService: BsModalService) { }
     ngOnInit() {
 
     }
-    //Opens 'sign up' modal that takes in a template of type 'ng-template'.
 
+    // Opens 'sign up' modal that takes in a template of type 'ng-template'.
     openModal(template: TemplateRef<any>) {
         this.modalRef = this.modalService.show(template);
     }

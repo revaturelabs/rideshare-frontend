d6ea8d3ba0f86cc277b1886f6a2cea38675a34b2
Inioluwa OluAlakija <inioluwa4@gmail.com>
2020-04-04 14:09:07 -0400

    register/login

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -11,15 +11,15 @@ exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     // './src/**/*.e2e-spec.ts'
-    './test/landingpage.spec.js',
+    // './test/homepage.spec.js',
     './test/register.spec.js',
-    './test/usernav.spec.js'
+    // './test/usernav.spec.js'
   ],
   capabilities: {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://http://54.174.82.153:4200/',
+  baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,

d2fb1d09a1417dc6219d9ddc4780339ed634f3f3
EvilBoyColor <nintendoblood7@gmail.com>
2020-04-07 18:24:43 -0400

    In this commit, I implemented some basic 'required' validators and set up the submit button with its 'onSubmit' method.  It only prints the FormGroup object to the console for now.  This is so that we can all see how valuable the reactive approach is..  More details in comments of the code. - Collin H

--- src/app/components/sign-up-modal/sign-up-modal.component.ts
+++ src/app/components/sign-up-modal/sign-up-modal.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef} from 'ngx-bootstrap';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 
-/*** Other than the changes in the template, I imported ReactiveFormsModule into the 
-  app module, then imported FormGroup and FormControl here in this component.
-  I've also implemented the synchronization with the template items below.
+/*** In this commit, I really only implemented the onSubmit method 
+  (called by the submit button of the FormGroup) and a few basic 
+  built-in validators.
 
-  NOTE: I will have to look up how to approach the button group for the radio input
-  field. To do this, I can check out what I did in my project II; otherwise, we will
-  need to change the absolutely beautiful, timeless button group formatting to normal
-  bootstrap styling.. what a fucking tragedy that would be...
-
-  For now, the button group is not included in the controls.
-***/
+  NOTE: We are not calling the methods in the constructors, we are
+  simply passing a reference to the methods, which gives angular 
+  access to them when the user tries to submit the forms.
+  
+  TLDR: IF YOU WANT TO ADD A VALIDATOR, IT WONT WORK IF YOU CALL THE 
+  METHOD DIRECTLY.
+ ***/
 
 
 
@@ -25,36 +25,48 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class SignupModalComponent implements OnInit {
 
-  //This 'signUpForm' will be the reference to our reactive form.
-  signUpForm: FormGroup;
+  signUpForm: FormGroup;//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< REMEMBER: This is the reference to the form.
   modalRef :BsModalRef;
   
   constructor(private modalService :BsModalService) { }
 
+  /*** Check out the object that is emitted int hte web console if you want 
+    to see how this is a big step forward.
+
+    Notice that the 'controls' attribute of the object contains many details
+    about the submitted form that is being created programmatically. It's 
+    much more detailed than the template driven approach, and whatever the 
+    hell we were working with before.
+   ***/
   ngOnInit() {
-    //This will be where we configure the form controls.
     this.signUpForm = new FormGroup({
-      //Here, we will add any and all FormControls (AKA. inputs) that exist within our FormGroup (AKA. form).
-      //They should be identified by their 'formControlName' attributes that we set in the template like this:
-      'firstname': new FormControl(null),
-      'lastname': new FormControl(null),
-      'email': new FormControl(null),
-      'phonenumber': new FormControl(null),
-      'batch': new FormControl(null),
-      'address': new FormControl(null),
-      'city': new FormControl(null),
-      'state': new FormControl(null),
-      'zipcode': new FormControl(null),
-      'username': new FormControl(null),
-      'password': new FormControl(null)
-      //I hope that these match whatever model we will be using to stringify/parse for sending off the form...
-      //With this implementation, we can add arrays of validators/conditions if we like, including custom ones.
-      //The constructor's first parameter is the default value, so I just set it to null for now.
+      //Remember that the first parameter to this constructor is the default 
+      //value. It could be anything, I've only set it to null here. Also 
+      //remmeber that we can add as many validators as we please, including 
+      //those which we create ourselves.
+      'firstname': new FormControl(null, Validators.required),
+      'lastname': new FormControl(null, Validators.required),
+      'email': new FormControl(null, Validators.required),
+      'phonenumber': new FormControl(null, Validators.required),
+      'batch': new FormControl(null, Validators.required),
+      'address': new FormControl(null, Validators.required),
+      'city': new FormControl(null, Validators.required),
+      'state': new FormControl(null, Validators.required),
+      'zipcode': new FormControl(null, Validators.required),
+      'username': new FormControl(null, Validators.required),
+      'password': new FormControl(null, Validators.required)
     })    
   }
 
   openModal(template :TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
+  }
+
+  //NOTE: We can access the signUpForm from any method because it is a 
+  //declared reference in this component. Angular is basically taking care 
+  //of the form input and validation for us, we just have to implement and configure.
+  onSubmit() {
+    console.log(this.signUpForm);
   }
 
 }

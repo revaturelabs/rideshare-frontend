cc48a847f458a676d6c0d096d632255675ed305d
EvilBoyColor <nintendoblood7@gmail.com>
2020-04-07 16:59:11 -0400

    In this commit, I brought in the ReactiveFormsModule, imported from that module the FormGroup and FormControl classes, and implemented a synchronized form between the template and component- ready to be configured with validation items. Read comments for more info. - Collin H

--- src/app/components/sign-up-modal/sign-up-modal.component.ts
+++ src/app/components/sign-up-modal/sign-up-modal.component.ts
@@ -1,5 +1,22 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef} from 'ngx-bootstrap';
+import { FormGroup, FormControl } from '@angular/forms';
+
+
+
+/*** Other than the changes in the template, I imported ReactiveFormsModule into the 
+  app module, then imported FormGroup and FormControl here in this component.
+  I've also implemented the synchronization with the template items below.
+
+  NOTE: I will have to look up how to approach the button group for the radio input
+  field. To do this, I can check out what I did in my project II; otherwise, we will
+  need to change the absolutely beautiful, timeless button group formatting to normal
+  bootstrap styling.. what a fucking tragedy that would be...
+
+  For now, the button group is not included in the controls.
+***/
+
+
 
 @Component({
   selector: 'signupmodal',
@@ -8,22 +25,34 @@ import { BsModalService, BsModalRef} from 'ngx-bootstrap';
 })
 export class SignupModalComponent implements OnInit {
 
-  /*** To continue using the bootstrap css modal, we must keep this reference to it;
-    ideally, we can create an alternative directive, but to mitigate the amount of
-    refactoring we are undertaking for now, it's got to be kept.
-  ***/
+  //This 'signUpForm' will be the reference to our reactive form.
+  signUpForm: FormGroup;
   modalRef :BsModalRef;
   
   constructor(private modalService :BsModalService) { }
 
   ngOnInit() {
-    
+    //This will be where we configure the form controls.
+    this.signUpForm = new FormGroup({
+      //Here, we will add any and all FormControls (AKA. inputs) that exist within our FormGroup (AKA. form).
+      //They should be identified by their 'formControlName' attributes that we set in the template like this:
+      'firstname': new FormControl(null),
+      'lastname': new FormControl(null),
+      'email': new FormControl(null),
+      'phonenumber': new FormControl(null),
+      'batch': new FormControl(null),
+      'address': new FormControl(null),
+      'city': new FormControl(null),
+      'state': new FormControl(null),
+      'zipcode': new FormControl(null),
+      'username': new FormControl(null),
+      'password': new FormControl(null)
+      //I hope that these match whatever model we will be using to stringify/parse for sending off the form...
+      //With this implementation, we can add arrays of validators/conditions if we like, including custom ones.
+      //The constructor's first parameter is the default value, so I just set it to null for now.
+    })    
   }
 
-  /*** This is where we are using the modal reference, so that the modal appears when we click "Sign Up".
-    The method 'openModal()' is called when the user clicks "Sign Up".
-    It receives a reference to the template, '#template', which contains our FormGroup.
-  ***/
   openModal(template :TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }

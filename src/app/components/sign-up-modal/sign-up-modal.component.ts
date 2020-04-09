a9dfd893b85982f879f9e195d7ade71f06eb65f3
rgyoungideas@gmail.com <rgyoungideas@gmail.com>
2020-04-09 14:00:23 -0400

    Mediated commuication discepencies on the signup modal as a group to bring the code back to an agreed state that also implements validation service along with angular validators to be able to use the custom services in other compnents down the line. Got auto-complete functioning as should, submit works as should to be able to evolve into data flow across components and reverted form.builder back to 'new FormGroup' syntax for sake of previous working conflicts

--- src/app/components/sign-up-modal/sign-up-modal.component.ts
+++ src/app/components/sign-up-modal/sign-up-modal.component.ts
@@ -24,7 +24,7 @@ export class SignupModalComponent implements OnInit {
   submitted = false;
   signUpForm: FormGroup;
   modalRef :BsModalRef;
-  apiKey;
+
   //This is where we are storing the address taken from the API.
   formattedAddress: string;
 
@@ -47,39 +47,53 @@ export class SignupModalComponent implements OnInit {
   //   s.text = ``;
   //   this.renderer2.appendChild(this._document.body, s);
    
-    this.signUpForm = this.formBuilder.group({
-      'firstname': ['',[ 
-        Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(35), 
-        ValidationService.stringValidator
-      ]],
-      'lastname':['', [
-        Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(35), 
-        ValidationService.stringValidator
-      ]],
-      'email': ['',[
-        Validators.required,
-        ValidationService.emailValidator  
-      ]],
-      'phonenumber': ['',[
-        Validators.required,
-        Validators.minLength(10) ,
-        ValidationService.phoneNumberValidator
-      ]],
-      'batch': ['', Validators.required],
-      'address': ['', Validators.required],
-      'city': ['', Validators.required],
-      'state': ['', Validators.required],
-      'zipcode': ['', Validators.required],
-      'username':['', Validators.required],
-      'password': ['', Validators.required],
-    })    
-  }
+  //   this.signUpForm = this.formBuilder.group({
+  //     'firstname': ['',[ 
+  //       Validators.required,
+  //       Validators.minLength(5),
+  //       Validators.maxLength(35), 
+  //       ValidationService.stringValidator
+  //     ]],
+  //     'lastname':['', [
+  //       Validators.required,
+  //       Validators.minLength(5),
+  //       Validators.maxLength(35), 
+  //       ValidationService.stringValidator
+  //     ]],
+  //     'email': ['',[
+  //       Validators.required,
+  //       ValidationService.emailValidator  
+  //     ]],
+  //     'phonenumber': ['',[
+  //       Validators.required,
+  //       Validators.minLength(10) ,
+  //       ValidationService.phoneNumberValidator
+  //     ]],
+  //     'batch': ['', Validators.required],
+  //     'address': ['', Validators.required],
+  //     'city': ['', Validators.required],
+  //     'state': ['', Validators.required],
+  //     'zipcode': ['', Validators.required],
+  //     'username':['', Validators.required],
+  //     'password': ['', Validators.required],
+  //   })    
+  // }
+  this.signUpForm = new FormGroup({
+    'firstname': new FormControl('', Validators.required),
+    'lastname': new FormControl('', Validators.required),
+    'email': new FormControl('', Validators.required),
+    'phonenumber': new FormControl('', [Validators.required,ValidationService.phoneNumberValidator]),
+    'batch': new FormControl('', Validators.required),
+    'address': new FormControl('', Validators.required),
+    'city': new FormControl('', Validators.required),
+   'state': new FormControl('', Validators.required),
+    'zipcode': new FormControl('', Validators.required),
+    'username': new FormControl('', Validators.required),
+    'password': new FormControl('', Validators.required)
+  })    
+}
 
-  get f() { return this.signUpForm.controls; }
+  // get f() { return this.signUpForm.controls; }
 
   openModal(template :TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
@@ -93,11 +107,13 @@ export class SignupModalComponent implements OnInit {
     //You can see that we are setting our own variable 'formattedAddress' 
     //equal to the value of the API's formattedAddress.
     this.formattedAddress = address.formattedAddress;
+
+
   }
 
   onSubmit() {
-    this.submitted = true;
-
+    // this.submitted = true;
+    console.log("welcome!"+this.signUpForm.value.firstname);
         // stop here if form is invalid
         if (this.signUpForm.invalid) {
             return;
@@ -105,7 +121,7 @@ export class SignupModalComponent implements OnInit {
 
         // display form values on success
         alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.signUpForm.value, null, 4));
-    console.log(this.signUpForm);
+    
   }
 
 }

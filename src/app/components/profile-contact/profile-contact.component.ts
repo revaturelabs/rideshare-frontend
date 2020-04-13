8780e83a9efe45b03fae81cb48d1cdf84f2b4e89
RaiBear08 <61160598+RaiBear08@users.noreply.github.com>
2020-04-13 12:57:25 -0700

    refactored profile location and contact component. added form group and some validators

--- src/app/components/profile-contact/profile-contact.component.ts
+++ src/app/components/profile-contact/profile-contact.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user-service/user.service';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
-
+import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-profile-contact',
   templateUrl: './profile-contact.component.html',
@@ -17,19 +18,36 @@ export class ProfileContactComponent implements OnInit {
   email: string;
   phone: string;
   success :string;
+  profileContact = new FormGroup({
+    firstName: new FormControl("", Validators.required),
+    lastName: new FormControl("", Validators.required),
+    email: new FormControl("", Validators.required ),
+    phone: new FormControl("", Validators.required )
+  });
+
+
   constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {
-    this.currentUser = this.userService.getUserById2(sessionStorage.getItem("userid")).subscribe((response)=>{
-      this.profileObject = response;
+     this.userService.getUserById2(sessionStorage.getItem("userid")).subscribe((response)=>{
+     
+      if(response != null){
+        this.profileContact = new FormGroup({
+          firstName: new FormControl(response.firstName, Validators.required),
+          lastName: new FormControl(response.lastName, Validators.required),
+          email: new FormControl(response.email, Validators.required),
+          phone: new FormControl(response.phoneNumber, Validators.required)
+        });
+      }
+      else{
+        this.success ="No user information is found"
+      }
 
-      this.firstName = this.profileObject.firstName;
-      this.lastName = this.profileObject.lastName;
-      this.email = this.profileObject.email;
-      this.phone = this.profileObject.phoneNumber;
+this.currentUser = response;
+    }, error=>{
 
     });
-    
+
   }
 
   updatesContactInfo(){
@@ -37,10 +55,14 @@ export class ProfileContactComponent implements OnInit {
     this.profileObject.lastName = this.lastName;
     this.profileObject.email = this.email;
     this.profileObject.phoneNumber = this.phone;
-
     this.userService.updateUserInfo(this.profileObject);
     this.success = "Updated Successfully!";
   }
 
+  get validInput(){
+    return this.profileContact.controls;
+  }
+
+  
 
 }
